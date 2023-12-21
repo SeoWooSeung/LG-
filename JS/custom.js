e143416b7a4044bf357cc9f53bb36a32d67d2222
서우승 <148739245+SeoWooSeung@users.noreply.github.com>
2023-12-21 18:15:59 +0900

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        dots: false,
+        fade: true,
+    });
+    $('.img_slide').slick({
+        arrows: false,
+        autoplay: false,
+        pauseOnHover: false,
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
+
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+    $('.main_visual .main_slide').on('afterChange', function (e, s, c) {
+        console.log(e, s, c);//c=0,1
+        $('.main_visual .num span').text('0' + (c + 1));
+    })
+
+    
+
+
+    
+});
